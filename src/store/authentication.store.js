ec187901e27daee29b7e69db68ca2ec3b832ba56
robert <robertreji2005@gmail.com>
2026-03-12 00:07:13 +0530

    added admin user

--- src/store/authentication.store.js
+++ src/store/authentication.store.js
@@ -2,7 +2,7 @@ import { create } from 'zustand'
 
 const useAuthenticate = create((set) => ({
   isAuthenticated: false,
-  adminUsername: ["@adarshhtaman:matrix.org"],
+  adminUsername: ["@adarshhtaman:matrix.org",""],
   logedInUser: null,
   authenticate: (user) => 
     set({ isAuthenticated: true, logedInUser: user }),
